0145b6fba563f2a45594946cbf52da115fafe0da
Kirill Bobkov <kirill_2105@mail.ru>
2024-12-29 14:49:10 +0400

    img for preview 2

--- components/Bio/Bio.jsx
+++ components/Bio/Bio.jsx
@@ -8,11 +8,12 @@ export const Bio = () => {
   const [loaded, setLoaded] = React.useState(false);
   return (
     <section className={styles.block} id="about">
-          <VisibilityManager side="left" as="h2" className={styles.title}>
-              Александр <br/><span className={styles.title_empty}>Васильев</span>
-            </VisibilityManager>
+      <VisibilityManager side="left" as="h2" className={styles.title}>
+        Александр <br />
+        <span className={styles.title_empty}>Васильев</span>
+      </VisibilityManager>
       <div className={styles.block__container}>
-      <VisibilityManager  className={styles.imageWrapper}>
+        <VisibilityManager className={styles.imageWrapper}>
           <Image
             src={alex3}
             alt="Александр Васильев"
@@ -22,28 +23,33 @@ export const Bio = () => {
         </VisibilityManager>
         <div className={styles.textContainer}>
           <div className={styles.textWrapper}>
-            <VisibilityManager  side="right" delay={0.5}  as="blockquote" className={styles.uptitle}>
+            <VisibilityManager
+              side="right"
+              delay={0.5}
+              as="blockquote"
+              className={styles.uptitle}
+            >
               Я устраняю причины Ваших страданий навсегда.
             </VisibilityManager>
             <VisibilityManager delay={0.5} className={styles.description}>
               <p>
-                Вам не нравится то, как Вы живёте и чувствуете себя? Я помогу Вам обрести то состояние, о котором Вы мечтаете.
+                Вам не нравится то, как Вы живёте и чувствуете себя? Я помогу
+                Вам обрести то состояние, о котором Вы мечтаете.
               </p>
-              <br/>
+              <br />
               <p>
                 Используя свои навыки, я смог улучшить жизни множества людей и
                 смогу помочь Вам, даже если сейчас Вы в это не верите.
               </p>
-              <br/>
+              <br />
               <p>
                 Запишитесь на благотворительную консультацию и узнайте истинные
-                причины проблем, способы решения и возможные преграды на пути к Вашему
-                процветанию!
+                причины проблем, способы решения и возможные преграды на пути к
+                Вашему процветанию!
               </p>
             </VisibilityManager>
           </div>
         </div>
-      
       </div>
     </section>
   );
