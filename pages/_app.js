b82c683372a98c4f4ce528254f5d161846ce42e7
Kirill Bobkov <kirill_2105@mail.ru>
2025-03-23 18:26:16 +0300

    yandex metrika

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/reset.css";
 import "../styles/global.css";
 import { ThemeProvider } from "../hooks/useTheme";
+import MetricsCode from "../components/MetricsCode";
 
 export default function({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <MetricsCode />
     </ThemeProvider>
   );
 }
