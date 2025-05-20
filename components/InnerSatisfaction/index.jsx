994407ee3462c0e664e013eb9403248d3b8569c2
Kirill Bobkov <kirill_2105@mail.ru>
2025-05-20 23:38:32 +0300

    Update styles and content in CirclesList, InnerQuestions, and InnerSatisfaction components
    - Reduced font sizes in CirclesList.module.css for better visual hierarchy.
    - Updated questions and descriptions in InnerQuestions and InnerSatisfaction for improved clarity and engagement.
    - Enhanced messaging in InnerSatisfaction to resonate with users seeking personal growth and understanding.
    - Added min-width to circles in CirclesList for consistent layout.
    - Modified sitemap.xml to include the main site URL with updated metadata.

--- components/InnerSatisfaction/index.jsx
+++ components/InnerSatisfaction/index.jsx
@@ -5,30 +5,43 @@ import contentStyles from "../../styles/contentStyles.module.css";
 
 export const InnerSatisfaction = () => {
   return (
-      <div className={styles.container}>
-        <div className={styles.block}>
-          <VisibilityManager
-            as="h2"
-            side="left"
-            className={contentStyles.title}
-            style={{ marginLeft: 0, marginRight: 0, marginBottom: 0 }}
-          >
-            <span>ВЕДУ ЛЮДЕЙ   К ИСТИННОМУ «Я» </span>
-            <span className={contentStyles.title_empty}>
-          И СОЗДАТЕЛЯМ
-            </span>
-          </VisibilityManager>
+    <div className={styles.container}>
+      <div className={styles.block}>
+        <VisibilityManager
+          as="h2"
+          side="left"
+          className={contentStyles.title}
+          style={{ marginLeft: 0, marginRight: 0, marginBottom: 0 }}
+        >
+          <span>
+            <span className={contentStyles.title_empty}>ЧТО </span>Я ПРЕДЛАГАЮ?
+          </span>
+        </VisibilityManager>
 
-          <VisibilityManager>
-            <p className={styles.paragraph} style={{ maxWidth: 700 }}>
-              Каждый человек подсознательно стремится к двум целям: обрести себя и соединиться с Творцами.
-              <br /><br />
-              Однако, в вихре амбиций и ответственности деловые люди нередко теряют внутренний ориентир.
-              <br /><br />
-              Выгорание, утрата смысла, финансовый потолок и проблемы со здоровьем всё чаще становятся спутниками внешнего успеха. Эти вызовы сигнализируют о разрыве с подлинной сутью.
-            </p>
-          </VisibilityManager>
-        </div>
+        <VisibilityManager>
+          <p className={styles.paragraph} style={{ maxWidth: 700 }}>
+            Если вы чего-то достигли, но потеряли смысл жизни, разочаровались в
+            людях, регулярно испытываете нервозность, апатию или суицидальные
+            мысли, не переживайте. Не всё потеряно.
+            <br />
+            <br />
+            Я знаю как вам помочь, и у вас есть бесплатный способ убедиться в
+            этом. Что я имею ввиду? Я занимаюсь нейротраблшутингом (точечной
+            перезагрузкой мышления через подсознание, работой с причинами
+            внутренних тормозов, которые невозможно увидеть логикой) и, в случае
+            отсутствия результата, возвращаю деньги.
+            <br />
+            <br />
+            Более того, кто хочет познакомиться с самим собой и кардинально
+            изменить свою внутреннюю составляющую, может записаться у меня на
+            мухоморный ретрит. Это не для всех, но кто чувствует зов, ни с чем
+            его не перепутает.
+            <br />
+            <br />
+            Добро пожаловать!
+          </p>
+        </VisibilityManager>
       </div>
+    </div>
   );
 };
