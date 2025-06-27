25cac2c3d7b10adc4231d8b9cfca7e9435b96b2b
Kirill Bobkov <kirill_2105@mail.ru>
2025-06-27 17:57:27 +0300

    Refactor PurchaseCard and ReasonCards styles for improved layout and consistency
    - Adjusted strokeWidth in PurchaseCard.jsx for better visual alignment.
    - Updated padding and positioning in PurchaseCard.module.css for enhanced layout consistency.
    - Removed unnecessary font-size and line-height properties in ReasonCards.module.css to streamline styles.
    - Modified input padding in Input.module.css for improved user experience.

--- components/PurchaseCard/PurchaseCard.jsx
+++ components/PurchaseCard/PurchaseCard.jsx
@@ -156,7 +156,7 @@ export const PurchaseCard = ({ onPaymentSubmit }) => {
                 <path
                   d="M15 18L9 12L15 6"
                   stroke="currentColor"
-                  strokeWidth="3"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
