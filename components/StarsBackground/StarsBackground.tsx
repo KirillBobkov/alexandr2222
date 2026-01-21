c63cc091b2f570b5fbff464d005f8b6c0b842697
Kirill Bobkov <kirill_2105@mail.ru>
2026-01-22 00:15:24 +0300

    Refactor StarsBackground component to simplify star rendering logic and remove unused CSS rules, enhancing performance and maintainability.

--- components/StarsBackground/StarsBackground.tsx
+++ components/StarsBackground/StarsBackground.tsx
@@ -1,6 +1,13 @@
 import React, { useMemo } from "react";
 import styles from "./StarsBackground.module.css";
 
+const TILE_SIZE = 2000;
+const TILE_OFFSETS = [
+  { x: 0, y: 0 },
+  { x: 0, y: -TILE_SIZE },
+  { x: TILE_SIZE, y: 0 },
+  { x: TILE_SIZE, y: -TILE_SIZE },
+];
 
 function generateBoxShadow(count: number, max = 2000) {
   const shadows: string[] = [];
@@ -13,16 +20,28 @@ function generateBoxShadow(count: number, max = 2000) {
 }
 
 export default function StarsBackground() {
-  const smallStars = useMemo(() => generateBoxShadow(300), []);
-  const mediumStars = useMemo(() => generateBoxShadow(100), []);
-  const bigStars = useMemo(() => generateBoxShadow(50), []);
+  const smallStars = useMemo(() => generateBoxShadow(300, TILE_SIZE), []);
+  const mediumStars = useMemo(() => generateBoxShadow(100, TILE_SIZE), []);
+  const bigStars = useMemo(() => generateBoxShadow(50, TILE_SIZE), []);
+
+  const renderLayer = (boxShadow: string, sizeClass: string) =>
+    TILE_OFFSETS.map((offset, index) => (
+      <div
+        key={`${sizeClass}-${index}`}
+        className={styles["stars"] + " " + sizeClass}
+        style={{
+          boxShadow,
+          left: `${offset.x}px`,
+          top: `${offset.y}px`,
+        }}
+      />
+    ));
 
   return (
     <div className={styles["stars-root"]}>
-      <div className={styles["stars"] + ' ' + styles["stars-small"]} style={{ boxShadow: smallStars }} />
-      <div className={styles["stars"] + ' ' + styles["stars-medium"]} style={{ boxShadow: mediumStars }} />
-      <div className={styles["stars"] + ' ' + styles["stars-big"]} style={{ boxShadow: bigStars }} />
-
+      {renderLayer(smallStars, styles["stars-small"])}
+      {renderLayer(mediumStars, styles["stars-medium"])}
+      {renderLayer(bigStars, styles["stars-big"])}
     </div>
   );
 }
