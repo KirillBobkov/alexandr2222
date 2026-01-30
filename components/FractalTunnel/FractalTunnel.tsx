e3954bcf24b9f3bf964b004cd8a2fe16a6efc955
Kirill Bobkov <kirill_2105@mail.ru>
2026-01-30 20:31:16 +0300

    fix

--- components/FractalTunnel/FractalTunnel.tsx
+++ components/FractalTunnel/FractalTunnel.tsx
@@ -53,7 +53,7 @@ const FractalTunnel = () => {
   const containerRef = React.useRef<HTMLDivElement | null>(null)
   const frameRef = React.useRef<number | null>(null)
 
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     const container = containerRef.current
     if (!container) return
 
